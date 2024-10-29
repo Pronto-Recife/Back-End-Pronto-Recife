db11389d043be5c16e5d2ab2a29ca0045c591df5
matheusleobelo <bipoladogamer@gmail.com>
2024-10-28 22:13:41 -0300

    Renomeação do controlador médico e demais mudanças

--- src/main/java/com/start/pronto_recife/Controllers/MedicoController.java
+++ src/main/java/com/start/pronto_recife/Controllers/MedicoController.java
@@ -1,27 +1,41 @@
 package com.start.pronto_recife.Controllers;
 
 import com.start.pronto_recife.DTOs.DTOMedico;
+import com.start.pronto_recife.Mapper.MedicoMapper;
 import com.start.pronto_recife.Models.MedicoModel;
 import com.start.pronto_recife.Repositories.MedicoRepository;
+import com.start.pronto_recife.Service.MedicoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class medicoController {
-private final MedicoRepository medicoRepository;
+private final MedicoMapper medicoMapper;
+private final MedicoService medicoService;
+
+
+
 
 @PostMapping("/medico")
-    public ResponseEntity<MedicoModel> saveMedico(@RequestBody @Valid DTOMedico dtoMedico){
-    var medicoModel = new MedicoModel();
-    BeanUtils.copyProperties(dtoMedico, medicoModel); //model mapper ou map struct
-    return ResponseEntity.status(HttpStatus.CREATED).body(medicoRepository.save(medicoModel));
+    public ResponseEntity<DTOMedico> saveMedico(@RequestBody @Valid DTOMedico dtoMedico){
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(medicoService.createMedico(dtoMedico));
 }
+
+//@GetMapping("/medico")
+//    public ResponseEntity<List<MedicoModel>> getAll(){
+//
+//    }
 
 }
