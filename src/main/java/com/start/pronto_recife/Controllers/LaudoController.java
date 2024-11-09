1b251f5563ed3afd1b01d9d4bfb546da27fc44cc
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2024-11-09 17:19:29 -0300

    adicionar GET, POST e PUT da entidade Laudo

---
+++ src/main/java/com/start/pronto_recife/Controllers/LaudoController.java
@@ -0,0 +1,36 @@
+package com.start.pronto_recife.Controllers;
+
+import com.start.pronto_recife.DTOs.DTOLaudo;
+import com.start.pronto_recife.Service.LaudoService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController("laudo")
+@RequiredArgsConstructor
+public class LaudoController {
+    private final LaudoService laudoService;
+    // GET controller
+    @GetMapping("/laudo")
+    public ResponseEntity<List<DTOLaudo>> getAllLaudos(){
+        return ResponseEntity.status(HttpStatus.OK).body(laudoService.findAll());
+    }
+    // PUT controller
+    @PutMapping("/laudo/{id}")
+    public ResponseEntity<DTOLaudo> updateLaudo(@PathVariable UUID id, @RequestBody @Valid DTOLaudo dtoLaudo){
+        DTOLaudo laudo = laudoService.updateLaudo(id, dtoLaudo);
+        return ResponseEntity.status(HttpStatus.OK).body(laudo);
+    }
+    // POST controller
+    @PostMapping("/laudo")
+    public ResponseEntity<DTOLaudo> saveLaudo(@RequestBody DTOLaudo dtoLaudo){
+        return ResponseEntity.status(HttpStatus.CREATED).body(laudoService.createLaudo(dtoLaudo));
+    }
+}
---
+++ src/main/java/com/start/pronto_recife/Service/LaudoService.java
@@ -0,0 +1,37 @@
+package com.start.pronto_recife.Service;
+
+import com.start.pronto_recife.DTOs.DTOLaudo;
+import com.start.pronto_recife.Mapper.LaudoMapper;
+import com.start.pronto_recife.Models.LaudoModel;
+import com.start.pronto_recife.Repositories.LaudoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class LaudoService {
+   private final LaudoMapper laudoMapper;
+   private final LaudoRepository laudoRepository;
+    // Service findAll laudos
+    public List<DTOLaudo> findAll(){
+        List<LaudoModel> laudos = laudoRepository.findAll();
+        return laudoMapper.listEntitytoListDTO(laudos);
+    }
+   // Service update laudo
+    public DTOLaudo updateLaudo(UUID id, DTOLaudo dtoLaudo){
+        LaudoModel existingLaudo = laudoRepository.findById(id).orElseThrow(() -> new RuntimeException("Id do Laudo não encontrado."));
+
+        LaudoModel updatedLaudo = laudoMapper.toModel(dtoLaudo);
+        updatedLaudo.setId(existingLaudo.getId());
+        laudoRepository.save(updatedLaudo);
+        return laudoMapper.toDTO(updatedLaudo);
+    }
+    // Service create new laudo
+    public DTOLaudo createLaudo(DTOLaudo dtoLaudo){
+        LaudoModel laudoModel = laudoRepository.save(laudoMapper.toModel(dtoLaudo));
+        return laudoMapper.toDTO(laudoModel);
+    }
+}
