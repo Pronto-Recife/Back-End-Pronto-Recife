39d2bd0d6f44405e7457e731d3548f81a8db21e9
Ben-SS1 <gandao2013@gmail.com>
2024-11-12 18:00:36 -0300

    CRUD completo do Estabelecimento

--- src/main/java/com/start/pronto_recife/Controllers/EstabelecimentoController.java
+++ src/main/java/com/start/pronto_recife/Controllers/EstabelecimentoController.java
@@ -5,17 +5,32 @@ import com.start.pronto_recife.Service.EstabelecimentoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController("estabelecimento")
 @RequiredArgsConstructor
 public class EstabelecimentoController {
     private final EstabelecimentoService estabelecimentoService;
 
-    @PostMapping("/register")
+    @PostMapping("/estabelecimento/register")
     public ResponseEntity<DTOEstabelecimento> registerEstabelecimento(@RequestBody DTOEstabelecimento dtoEstabelecimento){
         return ResponseEntity.status(HttpStatus.CREATED).body(estabelecimentoService.createEstabelecimento(dtoEstabelecimento));
+    }
+    @GetMapping("/estabelecimento/all")
+    public ResponseEntity<List<DTOEstabelecimento>> getAllEstabelecimentos(){
+        return ResponseEntity.status(HttpStatus.OK).body(estabelecimentoService.findAll());
+    }
+    @GetMapping("estabelecimento/find/{id}")
+    public ResponseEntity<DTOEstabelecimento> getEstabelecimento(@PathVariable(value = "id")UUID id){
+        DTOEstabelecimento estabelecimento = estabelecimentoService.findById(id);
+        return ResponseEntity.ok().body(estabelecimento);
+    }
+    @DeleteMapping("/estabelecimento/delete/{id}")
+    public ResponseEntity<Void> deleteEstabelecimento(@PathVariable UUID id){
+        estabelecimentoService.deleteEstabelecimento(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
--- src/main/java/com/start/pronto_recife/Service/EstabelecimentoService.java
+++ src/main/java/com/start/pronto_recife/Service/EstabelecimentoService.java
@@ -3,9 +3,14 @@ package com.start.pronto_recife.Service;
 import com.start.pronto_recife.DTOs.DTOEstabelecimento;
 import com.start.pronto_recife.Mapper.EstabelecimentoMapper;
 import com.start.pronto_recife.Models.EstabelecimentoModel;
+import com.start.pronto_recife.Models.PacienteModel;
 import com.start.pronto_recife.Repositories.EstabelecimentoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +25,19 @@ public class EstabelecimentoService {
         EstabelecimentoModel newEstabelecimento = estabelecimentoMapper.toModel(dtoEstabelecimento);
         estabelecimentoRepository.save(newEstabelecimento);
         return estabelecimentoMapper.toDTO(newEstabelecimento);
+    }
+    public List<DTOEstabelecimento> findAll(){
+        List<EstabelecimentoModel> estabelecimentos = estabelecimentoRepository.findAll();
+        return estabelecimentoMapper.listEntitytoListDTO(estabelecimentos);
+    }
+    public DTOEstabelecimento findById(UUID id){
+        EstabelecimentoModel estabelecimentoExists = estabelecimentoRepository.findById(id).orElseThrow(() ->
+                new RuntimeException("Estabelecimento não existe!"));
+        return estabelecimentoMapper.toDTO(estabelecimentoExists);
+    }
+    public void deleteEstabelecimento(UUID id){
+        EstabelecimentoModel estabelecimentoExists = estabelecimentoRepository.findById(id).orElseThrow(() ->
+                new RuntimeException("Estabelecimento não existe!"));
+        estabelecimentoRepository.delete(estabelecimentoExists);
     }
 }
