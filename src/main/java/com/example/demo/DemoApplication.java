0734bc86aaf24c0cb3970a5493256ccf03cbaf04
JoãoSaraiva <joaovictor250804@gmail.com>
2024-11-16 16:34:01 -0300

    Update ProntoRecifeApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.start.pronto_recife;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ProntoRecifeApplicationTests {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
