def3cd683c2a35d2e858469a665545252827437d
JoãoSaraiva <joaovictor250804@gmail.com>
2024-11-16 16:34:35 -0300

    Update MySpringBootApplication.java

--- src/main/java/com/example/demo/MySpringBootApplication.java
+++ src/main/java/com/example/demo/MySpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.start.pronto_recife;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
