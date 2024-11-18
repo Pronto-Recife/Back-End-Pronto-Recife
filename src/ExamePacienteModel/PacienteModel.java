845c4a426c9e6bbedd931f8c84a24aa6a2a6cfd1
JoãoSaraiva <joaovictor250804@gmail.com>
2024-11-18 03:55:02 -0300

    Add files via upload

---
+++ src/ExamePacienteModel/PacienteModel.java
@@ -0,0 +1,36 @@
+package com.start.pronto_recife.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "paciente")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PacienteModel{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(name = "cpf", unique = true, length = 14)
+    private String CPF;
+    private String nome_completo;
+    private LocalDate data_nascimento;
+    private String genero;
+    @Column(name = "email", unique = true)
+    private String email;
+    private String senha;
+    private String telefone;
+    private String contato_representante;
+    private String cep;
+    private String endereco;
+    @Column(name = "responsavel_id", length = 16)
+    private UUID responsavel_id;
+}
