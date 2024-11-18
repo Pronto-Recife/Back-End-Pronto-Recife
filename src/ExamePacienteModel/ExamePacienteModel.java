785781316dcddd097aec0d1ccce76833d38cb72b
JoãoSaraiva <joaovictor250804@gmail.com>
2024-11-18 03:39:35 -0300

    Update ExamePacienteModel.java

--- src/ExamePacienteModel/ExamePacienteModel.java
+++ src/ExamePacienteModel/ExamePacienteModel.java
@@ -1,78 +1,40 @@
-package com.start.pronto_recife.ExamePacienteModel;
+package com.start.pronto_recife.Models;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+
+import java.sql.Types;
+import java.time.LocalDate;
 import java.util.UUID;
-
 @Entity
 @Table(name = "exame_do_paciente")
-public class ExamePacienteModel {
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExamePacientModel {
 
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private UUID id;
+        @Id
+        @GeneratedValue(strategy =GenerationType.UUID)
+        @JdbcTypeCode(Types.VARCHAR)
 
-    @Column(name = "data_exame", nullable = false)
-    private String dataExame;
+        private UUID id;
 
-    @Column(name = "resultado", nullable = false)
-    private String resultado;
+        private LocalDate dataExame;
 
-    @Column(name = "nome_do_exame", nullable = false, length = 100)
-    private String nomeDoExame;
+        @Column(name = "resultado", nullable = false)
+        private String resultado;
 
-    @Column(name = "paciente_id", nullable = false)
-    private UUID pacienteId;
+        @Column(name = "nome_do_exame", nullable = false, length = 100)
+        private String nomeDoExame;
 
-    @Column(name = "consulta_id")
-    private UUID consultaId;
-}
+        @Column(name = "paciente_id", nullable = false)
+        private UUID pacienteId;
 
-//Getters e Setters
-public UUID getId() {
-    return id;
-}
-
-public void setId(UUID id) {
-    this.id = id;
-}
-
-public String getDataExame() {
-    return dataExame;
-}
-
-public void setDataExame(String dataExame) {
-    this.dataExame = dataExame;
-}
-
-public String getResultado() {
-    return resultado;
-}
-
-public void setResultado(String resultado) {
-    this.resultado = resultado;
-	}
-
-public String getNomeDoExame() {
-    return nomeDoExame;
-	}
-
-public void setNomeDoExame(String nomeDoExame) {
-    this.nomeDoExame = nomeDoExame;
-	}
-
-public UUID getPacienteId() {
-    return pacienteId;
+        @Column(name = "consulta_id")
+        private UUID consultaId;
     }
-
-public void setPacienteId(UUID pacienteId) {
-    this.pacienteId = pacienteId;
-	}
-
-public UUID getConsultaId() {
-    return consultaId;
-	}
-
-public void setConsultaId(UUID consultaId) {
-    this.consultaId = consultaId;
-	}
-}
