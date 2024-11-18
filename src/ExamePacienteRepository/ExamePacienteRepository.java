f5bd58fa4ee88a695a872abe161c52e3393cadd6
JoãoSaraiva <joaovictor250804@gmail.com>
2024-11-18 03:40:18 -0300

    Update ExamePacienteRepository.java

--- src/ExamePacienteRepository/ExamePacienteRepository.java
+++ src/ExamePacienteRepository/ExamePacienteRepository.java
@@ -1,10 +1,11 @@
-package com.start.pronto_recife.ExamePacienteRepository;
+package com.start.pronto_recife.Repositories;
 
+import com.start.pronto_recife.Models.ExamePacientModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.UUID;
+@Repository
+public interface ExamePacienteRepository extends JpaRepository<ExamePacientModel, UUID> {
 
-public interface ExamePacienteRepository extends JpaRepository<ExamePacienteRepository, UUID> {
-
-	<Exame> Exame save(Exame model);
-	
 }
