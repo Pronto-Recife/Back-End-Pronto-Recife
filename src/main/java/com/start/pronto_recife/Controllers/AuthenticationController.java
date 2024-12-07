f924adcd982566451e3098192e3a0cced46db314
matheusleobelo <bipoladogamer@gmail.com>
2024-12-07 10:05:39 -0300

    subindo railway

--- src/main/java/com/start/pronto_recife/Controllers/AuthenticationController.java
+++ src/main/java/com/start/pronto_recife/Controllers/AuthenticationController.java
@@ -7,10 +7,12 @@ import com.start.pronto_recife.Service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/authorization")
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
