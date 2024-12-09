d762e139d22c445d497f4d5a60d75e2c94863a8f
matheusleobelo <bipoladogamer@gmail.com>
2024-12-09 12:25:02 -0300

    codigo sem conflito

--- src/main/java/com/start/pronto_recife/DTOs/DTOConsulta.java
+++ src/main/java/com/start/pronto_recife/DTOs/DTOConsulta.java
@@ -3,7 +3,7 @@ package com.start.pronto_recife.DTOs;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 
-<<<<<<< HEAD
+
 import java.time.LocalDateTime; // Import correto para data e hora
 
 public record DTOConsulta(
@@ -11,14 +11,6 @@ public record DTOConsulta(
         @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
         LocalDateTime dataConsulta,
 
-=======
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
-public record DTOConsulta(
-        @NotNull  @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
-        LocalDateTime dataConsulta,
->>>>>>> 2d6e80ed13499512aa7f3f837757459543a64497
         @NotNull
         String tratamentosPrescritos,
 
